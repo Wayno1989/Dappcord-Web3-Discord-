2de754536278e4b20358283649e2446dd6461417
Wayne Kenny <wayne010406@gmail.com>
2024-07-25 11:23:43 +0100

    Final

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,10 +1,46 @@
-const Channels = ({ provider, account, dappcord, channels, currentChannel, setCurrentChannel }) => {
+const Channels = ({
+  provider,
+  account,
+  dappcord,
+  channels,
+  currentChannel,
+  setCurrentChannel,
+}) => {
+  const channelHandler = async (channel) => {
+    const hasJoined = await dappcord.hasJoined(channel.id, account);
 
+    if (hasJoined) {
+      setCurrentChannel(channel);
+    } else {
+      const signer = await provider.getSigner();
+      const transaction = await dappcord
+        .connect(signer)
+        .mint(channel.id, { value: channel.cost });
+      await transaction.wait();
+      setCurrentChannel(channel);
+    }
+  };
   return (
     <div className="channels">
       <div className="channels__text">
         <h2>Text Channels</h2>
 
+        <ul>
+          {channels.map((channel, index) => (
+            <li
+              key={index}
+              onClick={() => channelHandler(channel)}
+              className={
+                currentChannel &&
+                currentChannel.id.toString() === channel.id.toString()
+                  ? "active"
+                  : ""
+              }
+            >
+              {channel.name}
+            </li>
+          ))}
+        </ul>
       </div>
 
       <div className="channels__voice">
@@ -18,6 +54,6 @@ const Channels = ({ provider, account, dappcord, channels, currentChannel, setCu
       </div>
     </div>
   );
-}
+};
 
 export default Channels;
